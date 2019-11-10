e88b0b6dd3480f03450dddb8021116afc0bcbfaf
ecn3 <ecn3@students.uwf.edu>
2019-11-10 12:04:26 -0800

    I give up

--- Exercises/Exercise3/ex2.sql
+++ Exercises/Exercise3/ex2.sql
@@ -1,13 +1,12 @@
-drop table if exists 'ex2'.'movie';
+DROP TABLE IF EXISTS MOVIE_TABLE;
 
-CREATE TABLE 'ex2'.'movie'(
-'director' VARCHAR(20) NULL,
-'imdbRef' VARCHAR(10) NULL,
-'year' INT NOT NULL,
-'metascore' INT NOT NULL,
-'rottenTomatoes' INT NOT NULL,
-GROUP_CONCAT('keyword') 'keywords', VARCHAR(10) NULL,
-PRIMARY KEY ('imdbRef')
+CREATE TABLE [MOVIE_TABLE](
+   [director] [varchar](20) NOT NULL,
+   [imdbRef] [varchar](10) NOT NULL,
+   [year] [int] NOT NULL,
+   [metascore] [int] NOT NULL,
+   [rottenTomatoes] [int] NOT NULL,
+ CONSTRAINT [MOVIE_PK] PRIMARY KEY ([imdbRef])
 );
+GO
 
-select * from ex2.movie;
