362fb3c12c558b672483f07f632b80871bba6d9c
ecn3 <43419606+ecn3@users.noreply.github.com>
2019-11-20 17:25:07 -0600

    working sql

--- Projects/Project3/Project3ECN3/src/main/java/com/mycompany/project3ecn3/ECN3.sql
+++ Projects/Project3/Project3ECN3/src/main/java/com/mycompany/project3ecn3/ECN3.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS PlaylistECN3;
 
 CREATE TABLE PlaylistECN3 (
    directorName VARCHAR(20) NOT NULL,
-   directoBirthdate DATE,
+   directoBirthdate VARCHAR(10) NOT NULL,
    title VARCHAR(10) NOT NULL,
    runTime INT,
    productionCompany VARCHAR(20),
@@ -14,18 +14,16 @@ CREATE TABLE PlaylistECN3 (
    rottenTomatoes INT,
    keyword1 VARCHAR(20),
    keyword2 VARCHAR(20),
-   keyword3 VARCHAR(20),
-   keyword4 VARCHAR(20),
-   keyword5 VARCHAR(20)
    PRIMARY KEY (imdbRef)
 );
 
 
-INSERT INTO PlaylistECN3 VALUES ("Chris Columbus", TO_DATE('9/10/1958', 'MM/DD/YYYY'),"Harry Potter and the Chamber of Secrets",
+INSERT INTO PlaylistECN3 VALUES ("Chris Columbus", "9/10/1958","Harry Potter and the Chamber of Secrets",
 161,"Warner Bros.","tt0295297", 2002, 63, 82, "harrypotter", "jkrowlings");
 
-INSERT INTO PlaylistECN3 VALUES ("Zack Snyder", TO_DATE('3/1/1966', 'MM/DD/YYYY'),"Man of Steel",
+INSERT INTO PlaylistECN3 VALUES ("Zack Snyder", "3/1/1966","Man of Steel",
 143,"Warner Bros.","tt0770828", 2013, 55, 82, "manofsteel", "superman" );
 
-INSERT INTO PlaylistECN3 VALUES ("Matt Reeves", TO_DATE('04/27/1966', 'MM/DD/YYYY'),"Untitled Batman Reboot",
-Null,"Warner Bros.","tt1877830", 2021, Null, Null);
+
+INSERT INTO PlaylistECN3 VALUES ("Matt Reeves", "04/27/1966","Untitled Batman Reboot",
+122,"Warner Bros.","tt1877830", 2021, 0, 0, "batman", "robertpatterson");
