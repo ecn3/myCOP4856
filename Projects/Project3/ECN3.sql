5ec65107a77449d8c7cd04d5dde3f84b2ebf0575
Christian Nelson <ecn3@students.uwf.edu>
2019-11-20 14:20:22 -0600

    schema and playlist

--- Projects/Project3/ECN3.sql
+++ Projects/Project3/ECN3.sql
@@ -5,11 +5,14 @@ GO
 /*create table for tabale*/
 
 CREATE TABLE PlaylistECN3(
-   director VARCHAR(20) NOT NULL,
+   directorName VARCHAR(20) NOT NULL,
+   directoBirthdate DATE,
+   title VARCHAR(10) NOT NULL,
+   runTime INT,
    imdbRef VARCHAR(10) NOT NULL,
-   year INT NOT NULL,
-   metascore INT NOT NULL,
-   rottenTomatoes INT NOT NULL,
+   releaseyear INT NOT NULL,
+   metascore INT,
+   rottenTomatoes INT,
    keyword1 VARCHAR(20),
    keyword2 VARCHAR(20),
    keyword3 VARCHAR(20),
